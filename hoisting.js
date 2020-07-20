55f8af0526e769f49bfb200a557ee626789356a5
giorgi <giorgikutubidze@gmail.com>
2020-07-20 01:48:35 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -3,17 +3,19 @@ function callMe() {
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  return lyric;
+
   var lyric = "maybe";
+  return lyric;
 }
 
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
+
 
   var thisIsCrazy = function (){
-    console.log("hey!!!")
+    console.log("hey!!!");
+    thisIsCrazy();
   }
 }
 
@@ -24,7 +26,8 @@ function sayMyName() {
   sayMy();
 
   function sayMy() {
+
+     name = "Kristin";
     console.log(name);
-    var name = "Kristin";
   }
 }
